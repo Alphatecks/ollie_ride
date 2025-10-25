2387b1693a620664cfbec346bc4fbe92c143917a
Chikezie Ndubuisi <chikeziendubuisi7@gmail.com>
2025-10-25 01:54:36 +0100

    Fix tailwind.js: Replace @/ alias with relative path for tailwind.config.js

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,7 @@
 import { create } from 'twrnc';
 
 // create the customized version...
-const tw = create(require(`@/tailwind.config.js`)); // <- your path may differ
+const tw = create(require('./tailwind.config.js')); // <- using relative path
 
 // ... and then this becomes the main function your app uses
 export default tw;
