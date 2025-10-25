3bebb1af60663baf713cd5093c82d269c595de50
Chikezie Ndubuisi <chikeziendubuisi7@gmail.com>
2025-10-25 01:40:33 +0100

    Fix metro.config.js: Remove Expo dependencies, use React Native config

--- metro.config.js
+++ metro.config.js
@@ -1,25 +1,19 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg', 'cjs'],
+  },
+};
 
-const { getDefaultConfig } = require("expo/metro-config");
-
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
-
-  config.resolver.sourceExts.push('cjs');
-
-
-  const { transformer, resolver } = config;
-
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
-  };
-
-  return config;
-})();
-
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
